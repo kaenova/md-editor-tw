5fa8c48d48e1f323922d158b7ddfaac459d66186
Kaenova Mahendra Auditama <kaenova@gmail.com>
2022-01-20 16:18:42 +0700

    ok

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
 import '../styles/globals.css'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+  <Head>
+    <title>KMA: Markdown Editor</title>
+    <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    <link rel="icon" type="image/png" href="/Favicon.png" />
+  </Head>
+  <Component {...pageProps} />
+  </>
 }
 
 export default MyApp
